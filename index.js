49160d80b536f5945202f9d0d4e65c84602795b9
Shobham Rajak <shobham.rajak@gmail.com>
2022-12-14 11:44:30 +0530

    add rate limiter and cache

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
-
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+// rate limiter
+const limiter = rateLimit({
+    windowMS: 10*60*1000, // 10 minutes 
+    max: 100
+})
+
+app.use(limiter)
+app.set('trust proxy', 1)
+
+app.use('/api', require('./routes'))
 
 //enable cors
 app.use(cors());
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const url = require('url')
+const express = require('express')
+const router = express.Router()
+const needle = require('needle')
+const apicache = require('apicache')
+
+// env variables
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
+
+// init cache
+let cache = apicache.middleware
+
+router.get('/', cache('2 minutes'), async (req, res) => {
+    try {
+        const params = new URLSearchParams({
+            [API_KEY_NAME]: API_KEY_VALUE,
+            ...url.parse(req.url, true).query
+        })
+        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+        const data = apiRes.body
+        res.status(200).json(data)
+        if(process.env.NODE_ENV !== 'production') {
+            console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+        }
+    } catch (e) {
+        res.status(500).json(e)
+    }
+    
+})
+
+module.exports = router
